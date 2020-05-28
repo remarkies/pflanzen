85dab1e5f9c2b6b22a1a8756dd02393e059bad25
André Lergier <andre@lergier.ch>
2020-05-28 13:06:43 +0200

    feature: - multi dimensional comparison - sound comparison

---
+++ src/components/ComparisonSound.vue
@@ -0,0 +1,287 @@
+<template>
+  <div class="container-lg">
+    <Comparison :comparison-data="comparison" video is-two-dimensional />
+  </div>
+</template>
+
+<script>
+import Comparison from "@/components/Comparison";
+
+export default {
+  name: 'ComparisonSalad',
+  components: {
+    Comparison
+  },
+  data () {
+    return {
+      imagePath: 'salad',
+      comparison: [
+        {
+          title: 'Woche 1',
+          week: 19,
+          timeEntries: [
+            {
+              title: '9:00',
+              items: [
+                {
+                  videoReference: 'Ks80rsSehpU',
+                  place: 'Schmitten',
+                  person: 'Fabien'
+                },
+                {
+                  videoReference: 'Ks80rsSehpU',
+                  place: 'Hünibach',
+                  person: 'André'
+                },
+                {
+                  videoReference: 'Ks80rsSehpU',
+                  place: 'Adliswil',
+                  person: 'Luka'
+                },
+              ]
+            },
+            {
+              title: '16:00',
+              items: [
+                {
+                  videoReference: 'Ks80rsSehpU',
+                  place: 'Schmitten',
+                  person: 'Fabien'
+                },
+                {
+                  videoReference: 'Ks80rsSehpU',
+                  place: 'Hünibach',
+                  person: 'André'
+                },
+                {
+                  videoReference: 'Ks80rsSehpU',
+                  place: 'Adliswil',
+                  person: 'Luka'
+                },
+              ]
+            },
+            {
+              title: '23:00',
+              items: [
+                {
+                  videoReference: 'Ks80rsSehpU',
+                  place: 'Schmitten',
+                  person: 'Fabien'
+                },
+                {
+                  videoReference: 'Ks80rsSehpU',
+                  place: 'Hünibach',
+                  person: 'André'
+                },
+                {
+                  videoReference: 'Ks80rsSehpU',
+                  place: 'Adliswil',
+                  person: 'Luka'
+                },
+              ]
+            }
+          ]
+        },
+        {
+          title: 'Woche 2',
+          week: 19,
+          timeEntries: [
+            {
+              title: '9:00',
+              items: [
+                {
+                  videoReference: 'Ks80rsSehpU',
+                  place: 'Schmitten',
+                  person: 'Fabien'
+                },
+                {
+                  videoReference: 'Ks80rsSehpU',
+                  place: 'Hünibach',
+                  person: 'André'
+                },
+                {
+                  videoReference: 'Ks80rsSehpU',
+                  place: 'Adliswil',
+                  person: 'Luka'
+                },
+              ]
+            },
+            {
+              title: '16:00',
+              items: [
+                {
+                  videoReference: 'Ks80rsSehpU',
+                  place: 'Schmitten',
+                  person: 'Fabien'
+                },
+                {
+                  videoReference: 'Ks80rsSehpU',
+                  place: 'Hünibach',
+                  person: 'André'
+                },
+                {
+                  videoReference: 'Ks80rsSehpU',
+                  place: 'Adliswil',
+                  person: 'Luka'
+                },
+              ]
+            },
+            {
+              title: '23:00',
+              items: [
+                {
+                  videoReference: 'Ks80rsSehpU',
+                  place: 'Schmitten',
+                  person: 'Fabien'
+                },
+                {
+                  videoReference: 'Ks80rsSehpU',
+                  place: 'Hünibach',
+                  person: 'André'
+                },
+                {
+                  videoReference: 'Ks80rsSehpU',
+                  place: 'Adliswil',
+                  person: 'Luka'
+                },
+              ]
+            }
+          ]
+        },
+        {
+          title: 'Woche 3',
+          week: 19,
+          timeEntries: [
+            {
+              title: '9:00',
+              items: [
+                {
+                  videoReference: 'Ks80rsSehpU',
+                  place: 'Schmitten',
+                  person: 'Fabien'
+                },
+                {
+                  videoReference: 'Ks80rsSehpU',
+                  place: 'Hünibach',
+                  person: 'André'
+                },
+                {
+                  videoReference: 'Ks80rsSehpU',
+                  place: 'Adliswil',
+                  person: 'Luka'
+                },
+              ]
+            },
+            {
+              title: '16:00',
+              items: [
+                {
+                  videoReference: 'Ks80rsSehpU',
+                  place: 'Schmitten',
+                  person: 'Fabien'
+                },
+                {
+                  videoReference: 'Ks80rsSehpU',
+                  place: 'Hünibach',
+                  person: 'André'
+                },
+                {
+                  videoReference: 'Ks80rsSehpU',
+                  place: 'Adliswil',
+                  person: 'Luka'
+                },
+              ]
+            },
+            {
+              title: '23:00',
+              items: [
+                {
+                  videoReference: 'Ks80rsSehpU',
+                  place: 'Schmitten',
+                  person: 'Fabien'
+                },
+                {
+                  videoReference: 'Ks80rsSehpU',
+                  place: 'Hünibach',
+                  person: 'André'
+                },
+                {
+                  videoReference: 'Ks80rsSehpU',
+                  place: 'Adliswil',
+                  person: 'Luka'
+                },
+              ]
+            }
+          ]
+        },
+        {
+          title: 'Woche 4',
+          week: 19,
+          timeEntries: [
+            {
+              title: '9:00',
+              items: [
+                {
+                  videoReference: 'Ks80rsSehpU',
+                  place: 'Schmitten',
+                  person: 'Fabien'
+                },
+                {
+                  videoReference: 'Ks80rsSehpU',
+                  place: 'Hünibach',
+                  person: 'André'
+                },
+                {
+                  videoReference: 'Ks80rsSehpU',
+                  place: 'Adliswil',
+                  person: 'Luka'
+                },
+              ]
+            },
+            {
+              title: '16:00',
+              items: [
+                {
+                  videoReference: 'Ks80rsSehpU',
+                  place: 'Schmitten',
+                  person: 'Fabien'
+                },
+                {
+                  videoReference: 'Ks80rsSehpU',
+                  place: 'Hünibach',
+                  person: 'André'
+                },
+                {
+                  videoReference: 'Ks80rsSehpU',
+                  place: 'Adliswil',
+                  person: 'Luka'
+                },
+              ]
+            },
+            {
+              title: '23:00',
+              items: [
+                {
+                  videoReference: 'Ks80rsSehpU',
+                  place: 'Schmitten',
+                  person: 'Fabien'
+                },
+                {
+                  videoReference: 'Ks80rsSehpU',
+                  place: 'Hünibach',
+                  person: 'André'
+                },
+                {
+                  videoReference: 'Ks80rsSehpU',
+                  place: 'Adliswil',
+                  person: 'Luka'
+                },
+              ]
+            }
+          ]
+        },
+      ]
+    }
+  }
+}
+</script>
