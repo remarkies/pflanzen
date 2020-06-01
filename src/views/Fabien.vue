b8ac23197fe125bd93a9e72f1ba6a224d5779724
Fabien Jeckelmann <fabien.jeckelmann@gmail.com>
2020-06-01 10:48:07 +0200

    fruits + insects

--- src/views/Fabien.vue
+++ src/views/Fabien.vue
@@ -54,9 +54,51 @@ export default {
   },
   methods: {
     initArticles() {
+      const fruits = DataSet.createArticle(
+        "Früchte und Beeren",
+        "Wir verfügen in unserem über verschiedene Fruchtbäume und Beerensträucher, die uns mit frischen Produkten für Confiture, Desserts, Znüni/Zvieri etc. versorgen.<br><br>Um die Verwandlung von der Blüte zur Frucht festzuhalten, aber auch das Entwicklungsstadium der verschiedenen Pflazen zu vergleichen, habe ich die entsprechenden Pflanzen in den letzten Wochen fotografisch dokumentiert.",
+        [
+          DataSet.createImageSet("Unser <strong>Aprikosenbaum</strong> hat aufgrund des sehr frühen Frühlingeinbruchs bereits Ende März/Anfangs April geblüht. Ein späterer Kälteeinbruch hat ihm arg zugesetzt, aus welchem Grund dieses Jahr keine einzige Aprikose heranwächst.", false, [
+            DataSet.createRow([
+              DataSet.createImage("/img/fabien/fruits/P1020248.jpg", 12),
+            ])
+          ]),
+          DataSet.createImageSet("Der <strong>Zwetschgenbaum</strong> hat ein fruchtreiches Jahr hinter sich – jedes zweite jahr ist er besonders mit Früchten beladen. Dieses Jahr wachsen erwartungsgemäss weniger Zwetschgen heran. Ihre Entwicklung in den letzten Wochen ist auch eher subtil. Die charakteristische Form ist im Vergleich zu Dokumentationsbeginn ausgeprägter (siehe Rundungen auf Bild 2 + 3 im Kontrast zu Form in Bild 1) und das wachstende Gewicht der Frucht lässt sie vertikaler hängen.", false, [
+            DataSet.createRow([
+              DataSet.createImage("/img/fabien/fruits/P1020247.jpg", 12),
+              DataSet.createImage("/img/fabien/fruits/P1020287.jpg", 4),
+              DataSet.createImage("/img/fabien/fruits/P1020271.jpg", 4),
+              DataSet.createImage("/img/fabien/fruits/P1020272.jpg", 4),
+            ])
+          ]),
+          DataSet.createImageSet("<strong>Brombeeren</strong> erreichen bei uns ihre Reife zwischen Juli und August. In einem entsprechend frühen Entwicklungsstadium befinden sie sich aktuell. Immerhin ist die Evolution von der Blühte zur späteren Frucht bereits sichtbar.", false, [
+            DataSet.createRow([
+              DataSet.createImage("/img/fabien/fruits/P1020257.jpg", 4),
+              DataSet.createImage("/img/fabien/fruits/P1020303.jpg", 4),
+              DataSet.createImage("/img/fabien/fruits/P1020254.jpg", 4),
+            ])
+          ]),
+          DataSet.createImageSet("Die <strong>Himbeersträucher</strong> haben in den letzten Wochen eine unglaubliche Verwandlung durchlaufen. Von der verwelkten Blühte bis hin zu ersten rosafarbenen Früchten ist viel passiert.", false, [
+            DataSet.createRow([
+              DataSet.createImage("/img/fabien/fruits/P1020258.jpg", 4),
+              DataSet.createImage("/img/fabien/fruits/P1020305.jpg", 4),
+              DataSet.createImage("/img/fabien/fruits/P1020259.jpg", 4),
+            ])
+          ]),
+          DataSet.createImageSet("Bei Dokumentationsbeginn habe ich die <strong>Johannisbeeren</strong> – im Vergleich zu den anderen Früchten – bereits in einem fortgeschrittenen Stadium vorgefunden. Die Farbwandlung aus den letzten Wochen ist aber sehr gut sichtbar.", false, [
+            DataSet.createRow([
+              DataSet.createImage("/img/fabien/fruits/P1020316.jpg", 4),
+              DataSet.createImage("/img/fabien/fruits/P1020314.jpg", 4),
+              DataSet.createImage("/img/fabien/fruits/P1020312.jpg", 4),
+            ])
+          ])
+        ]
+      );
+      this.articles.push(fruits);
+
       const insects = DataSet.createArticle(
         "Makrofotografie",
-        "Insektenwelt des Gartens",
+        "Fotografie stellt in meinem Leben ein stiefmütterlich behandeltes Hobby dar. Ich finde verschiedenste Aspekte dieser Tätigkeit, von der Funktionsweise einer Kamera, über Bildkomposition bis hin zur Bildbearbeitung, sehr spannend und verbringe nichtdestotrotz nur sehr selten Zeit mit einer Fotokamera in den Händen.<br><br>Ein Bereich der Fotografie den ich bis anhin noch gar nie unter die Lupe genommen habe ist die Makrofotografie. Wie auch ohne Makroobjektiv oder externen Blitz! Genau dies habe ich mir zur Challenge gemacht: Was ist in diesem sehr spezifischen Bereich der Fotografie ohne spezifisches Equipment möglich?<br><br> <strong>Folgende Learnings habe ich bei meiner Arbeit gemacht:</strong> <ul><li>Vergrösserungsfaktor des Objektivs von 1:2 verunmöglicht das Fotografieren von sehr kleinen Insekten und Spinnen (< 1cm) im Gras</li><li>Kurzdistanziger Blitz würde dank kürzerer Belichtungszeit helfen, das Subjekt vom Hintergrund abzuheben (dünklerer Hintergrund)</li><li>Blitz verunmöglicht aufgrund der kurzen Belichtungszeit das scharfe Abbilden von fliegenden Insekten</li><li>Mindestfokusdistanz von 0.3m und Vergrösserungsfaktor von 1:2 reichen nicht, um fürs blosse Auge nicht sichtbare Details zu enthüllen. Einzig das Verwenden von sehr schnellen Verschlusszeiten, respektive das resultierende Einfrieren von Bewegung verhalf fürs menschliche Auge Verborgenes festzuhalten.</li><li>Um genügend Tiefenschärfe zu garantieren sind Blende f5.8 oder mehr ein Muss. Dadurch muss allerdings leider die ISO-Sensibilität erhöht werden, um genügend kurze Verschlusszeiten zu erreichen</li></ul><br>Als <strong>effektivstes Setup</strong> identifiziert habe ich: Maximaler Zoom, manueller Zoom auf die kleinste Fokusdistanz eingestellt, Blendenvorwahl, um Tiefenschärfe zu kontrollieren, ISO-Wert abhängig der resultierenden Belichtungszeit anpassen, Fokussieren durch Bewegen der ganzen Kamera.",
         [
           DataSet.createImageSet("", false, [
             DataSet.createRow([
