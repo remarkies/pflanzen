4007cfa8e27e1574a2d6ae2b4fff7321a9c397f7
remarkies <luka@ichbinich.ch>
2020-06-02 20:21:46 +0200

    Farbpalette Luka

--- src/components/ComparisonColorTheme.vue
+++ src/components/ComparisonColorTheme.vue
@@ -33,10 +33,10 @@ export default {
               colors: ["#e75232", "#fdd42c", "#fafafa", "#6b9412", "#242b3d"]
             },
             {
-              image: 'Farbpalette-Fabien.jpg',
-              person: 'Fabien',
-              rule: 'test',
-              colors: ["#e75232", "#fdd42c", "#fafafa", "#6b9412", "#242b3d"]
+              image: 'Farbpalette-Luka-2.jpg',
+              person: 'Luka',
+              rule: 'Der Farbverlauf der Blüten meiner Hortesie beginnt Innen mit einem dunkleren Grün und bewegt sich nach Aussen zu einem Rosa. Ziel war hier den Farbverlauf der Blüte mit 5 Farben darzustellen.',
+              colors: ["#596400", "#B6A000", "#F2D544", "#F28972", "#F26B6B"]
             },
           ]
         },
@@ -56,10 +56,10 @@ export default {
               colors: ["#e75232", "#fdd42c", "#fafafa", "#6b9412", "#242b3d"]
             },
             {
-              image: 'Farbpalette-Fabien.jpg',
-              person: 'Fabien',
-              rule: 'test',
-              colors: ["#e75232", "#fdd42c", "#fafafa", "#6b9412", "#242b3d"]
+              image: 'Farbpalette-Luka-1.jpg',
+              person: 'Luka',
+              rule: 'Hier bedarf es einer Eingrenzung, weil zu viele stark unterschiedliche Farben vorhanden sind. Die essentiellen Farben zum Bild liefert der Himmel und die verschiedenen Grüntöne der Pflanzen. Auf das Rot/Orange der Buschblätter, sowie das Rosa der Blüten musste verzichtet werden, weil sonst die Blautöne des Himmels oder die Grüntöne der Pflanzen zu kurz kommen würden. Ich habe mich für die Mehrheit der Blautöne entschieden, weil der Himmel hellere Farbtöne hergibt und somit als neutraler Hintergrund besser funktionieren würde.',
+              colors: ["#E2F2FA", "#C5E9F7", "#578399", "#7FA600", "#214C00"]
             },
           ]
         },
@@ -79,10 +79,10 @@ export default {
               colors: ["#e75232", "#fdd42c", "#fafafa", "#6b9412", "#242b3d"]
             },
             {
-              image: 'Farbpalette-Fabien.jpg',
-              person: 'Fabien',
-              rule: 'test',
-              colors: ["#e75232", "#fdd42c", "#fafafa", "#6b9412", "#242b3d"]
+              image: 'Farbpalette-Luka-3.jpg',
+              person: 'Luka',
+              rule: 'Das Ziel hier war die markanten Farben festzuhalten. Dazu gehörten die zwei Blütenfarben Gelb und Rosa und die drei grünen Abstufungen der Wiese.',
+              colors: ["#BC6E95", "#FFD500", "#78B400", "#316E00", "#053900"]
             },
           ]
         }
